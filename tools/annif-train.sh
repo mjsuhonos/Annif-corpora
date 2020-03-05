8f4dbc672c8f117576e213919f5d4a8da6465b03
MJ Suhonos <mj@suhonos.ca>
2020-03-05 16:40:46 -0500

    Fix UTF8 issues in source documents

--- tools/annif-train.sh
+++ tools/annif-train.sh
@@ -3,16 +3,18 @@
 # set environment variable for host IP
 export DOCKER_HOST=ssh://root@`doctl compute droplet list --format "Name,PublicIPv4" | grep docker-host | sed 's/[a-zA-Z<>/ :-]//g'`
 
-echo 'Training backends...'
+echo 'Loading vocabularies...'
 
 # Load vocabularies
-time docker exec -u root:root annif_bash_1 annif loadvoc rula-tfidf-en Annif-corpora/vocab/lcsh/lcsh.ttl
+time docker exec -u root:root annif_bash_1 annif loadvoc rula-maui-en Annif-corpora/vocab/lcsh/lcsh.ttl
+
+echo 'Training backends...'
 
 # Train individual backends in sequence
 time docker exec -u root:root annif_bash_1 annif train rula-tfidf-en Annif-corpora/training/rula.tsv.gz
 time docker exec -u root:root annif_bash_1 annif train rula-omikuji-parabel-en Annif-corpora/training/rula.tsv.gz
 time docker exec -u root:root annif_bash_1 annif train rula-maui-en Annif-corpora/fulltext/rula/validate/
-time docker exec -u root:root annif_bash_1 annif train rula-triple-ensemble-en Annif-corpora/training/rula.tsv.gz
+time docker exec -u root:root annif_bash_1 annif train rula-triple-ensemble-en Annif-corpora/fulltext/rula/validate/
 
 # unset environment variable for host IP
 unset DOCKER_HOST
