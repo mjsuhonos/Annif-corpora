5a901e155120ee7360288187201a594e687b99a5
MJ Suhonos <mj@suhonos.ca>
2020-03-09 14:45:17 -0400

    Create tsv2rdf.rb

---
+++ vocab/tsv2rdf.rb
@@ -0,0 +1,39 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'linkeddata'
+  gem 'rdf-vocab'
+  gem 'pry'
+end
+
+tsv_file, *files = ARGV
+
+exit if files.empty?
+exit if tsv_file.empty?
+exit unless File.exist? tsv_file
+
+
+
+skos_file = ARGV[0]
+abort 'ERROR: Please provide a SKOS RDF file to process' if skos_file.nil? || skos_file.empty?
+
+# intern the predicates we want
+pref_label = RDF::URI.intern RDF::Vocab::SKOS.prefLabel
+alt_label = RDF::URI.intern RDF::Vocab::SKOS.altLabel
+
+# open the output TSV files for writing
+pref_tsv = File.open("#{skos_file}-pref.tsv", "w")
+alt_tsv = File.open("#{skos_file}-alt.tsv", "w")
+
+RDF::Reader.open(skos_file) do |reader|
+  reader.each_statement do |statement|
+    tsv_line = "<#{statement.subject}>\t#{statement.object}"
+
+    if statement.predicate == pref_label then pref_tsv.puts tsv_line
+    elsif statement.predicate == alt_label then alt_tsv.puts tsv_line
+    end
+  end
+end
+
+exit
