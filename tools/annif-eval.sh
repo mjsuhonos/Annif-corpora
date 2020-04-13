cc7ac979384bb4e12662ccaef6bfb3b46ecde6a0
MJ Suhonos <mj@suhonos.ca>
2020-04-13 14:01:06 -0400

    Tidy train, eval scripts

--- tools/annif-eval.sh
+++ tools/annif-eval.sh
@@ -4,16 +4,16 @@
 export DOCKER_HOST=ssh://root@`doctl compute droplet list --format "Name,PublicIPv4" | grep docker-annif | sed 's/[a-zA-Z<>/ :-]//g'`
 
 echo ' ==> Evaluating backend: maui'
-docker exec -u root:root annif_bash_1 annif eval rula-maui-en Annif-corpora/fulltext/rula/test/ -v DEBUG
+docker exec -u root:root annif_bash_1 annif eval rula-maui-en Annif-corpora/fulltext/rula/test/
 
 echo ' ==> Evaluating backend: tf-idf'
-docker exec -u root:root annif_bash_1 annif eval rula-tfidf-en Annif-corpora/fulltext/rula/test/ -v DEBUG
+docker exec -u root:root annif_bash_1 annif eval rula-tfidf-en Annif-corpora/fulltext/rula/test/
 
 echo ' ==> Evaluating backend: omikuji-parabel'
-docker exec -u root:root annif_bash_1 annif eval rula-omikuji-parabel-en Annif-corpora/fulltext/rula/test/ -v DEBUG
+docker exec -u root:root annif_bash_1 annif eval rula-omikuji-parabel-en Annif-corpora/fulltext/rula/test/
 
 echo ' ==> Evaluating backend: nn-ensemble'
-docker exec -u root:root annif_bash_1 annif eval rula-nn-ensemble-en Annif-corpora/fulltext/rula/test/ -v DEBUG
+docker exec -u root:root annif_bash_1 annif eval rula-nn-ensemble-en Annif-corpora/fulltext/rula/test/
 
 # unset environment variable for host IP
 unset DOCKER_HOST
--- tools/annif-train.sh
+++ tools/annif-train.sh
@@ -12,7 +12,7 @@ docker exec -u root:root annif_bash_1 annif clear rula-omikuji-parabel-en
 docker exec -u root:root annif_bash_1 annif clear rula-nn-ensemble-en
 
 echo ' ==> Loading vocabularies...'
-#time docker exec -u root:root annif_bash_1 annif loadvoc rula-maui-en Annif-corpora/vocab/rula/rula-lcsh.ttl
+time docker exec -u root:root annif_bash_1 annif loadvoc rula-maui-en Annif-corpora/vocab/rula/rula-lcsh.ttl
 
 # Train individual backends in sequence
 echo ' ==> Training backends...'
@@ -20,7 +20,7 @@ echo ' ==> Training backends...'
 time docker exec -u root:root annif_bash_1 annif train rula-maui-en Annif-corpora/fulltext/rula/1000/ -v DEBUG
 time docker exec -u root:root annif_bash_1 annif train rula-tfidf-en Annif-corpora/training/rula.tsv.gz -v DEBUG
 time docker exec -u root:root annif_bash_1 annif train rula-omikuji-parabel-en Annif-corpora/training/rula.tsv.gz -v DEBUG
-time docker exec -u root:root annif_bash_1 annif train rula-nn-ensemble-en Annif-corpora/fulltext/rula/1000/ -v DEBUG
+time docker exec -u root:root annif_bash_1 annif train rula-nn-ensemble-en Annif-corpora/fulltext/rula/1000/
 
 # unset environment variable for host IP
 unset DOCKER_HOST
