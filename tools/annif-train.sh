55318b06755e6d13114d174846fa3bbcf0ed2a8f
MJ Suhonos <142849+mjsuhonos@users.noreply.github.com>
2021-07-23 13:39:00 -0400

    Split train and clear scripts

--- tools/annif-train.sh
+++ tools/annif-train.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# Clear Annif to initial state
-echo ' ==> Resetting Annif projects...'
-
-docker exec -u root:root annif_bash_1 annif clear rula-maui-en
-docker exec -u root:root annif_bash_1 annif clear rula-tfidf-en
-docker exec -u root:root annif_bash_1 annif clear rula-omikuji-parabel-en
-docker exec -u root:root annif_bash_1 annif clear rula-fasttext-en
-docker exec -u root:root annif_bash_1 annif clear rula-mllm-en
-docker exec -u root:root annif_bash_1 annif clear rula-stwfsa-en
-docker exec -u root:root annif_bash_1 annif clear rula-yake-en
-docker exec -u root:root annif_bash_1 annif clear rula-vw-multi-en
-docker exec -u root:root annif_bash_1 annif clear rula-svc-en
-docker exec -u root:root annif_bash_1 annif clear rula-nn-ensemble-en
-
-echo ' ==> Loading vocabularies...'
-time docker exec -u root:root annif_bash_1 annif loadvoc rula-maui-en Annif-corpora/vocab/rula/rula-lcsh.ttl
-
 # Train individual backends in sequence
 echo ' ==> Training backends...'
 
