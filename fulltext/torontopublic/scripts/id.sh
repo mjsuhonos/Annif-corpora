241b3294aa8d88b223f0eb23981c67bc8e718140
MJ Suhonos <142849+mjsuhonos@users.noreply.github.com>
2025-12-27 14:55:45 -0500

    Delete fulltext directory

--- fulltext/torontopublic/scripts/id.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-idloc get https://id.loc.gov/authorities/subjects/$1 | jq '.["skos:prefLabel"]["@value"]'
--- fulltext/torontopublic/scripts/qid.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-curl -s https://www.wikidata.org/wiki/$1 | rg wikibase-title-label | sed -E 's/(<span class="wikibase-title-label">([^<]+)<\/span>[\n\r]*)*/"\2"/g' | uniq | jq
